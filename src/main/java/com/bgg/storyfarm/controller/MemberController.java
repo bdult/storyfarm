a1e5a5e4336fcd7b9859eebcecf0ec5795bf48ec
soulmatt <qotkwk@gmail.com>
2013-08-20 15:48:26 +0900

    Update 6p

--- src/main/java/com/bgg/storyfarm/controller/MemberController.java
+++ src/main/java/com/bgg/storyfarm/controller/MemberController.java
@@ -13,9 +13,9 @@ public class MemberController {
 	
 	private Logger logger = LoggerFactory.getLogger(MemberController.class);
 	
-	@RequestMapping(value = "login.do", method = RequestMethod.GET)
-	public String loginpage(Model model) {
-		return "member/login";
+	@RequestMapping(value = "loginStep1.do", method = RequestMethod.GET)
+	public String loginStep1(Model model) {
+		return "member/loginStep1";
 	}
 	
 	@RequestMapping(value = "leave.do", method = RequestMethod.GET)
@@ -31,6 +31,11 @@ public class MemberController {
 	@RequestMapping(value = "joinStep2.do", method = RequestMethod.GET)
 	public String joinStep2(Model model) {
 		return "member/joinStep2";
+	}
+	
+	@RequestMapping(value = "loginStep2.do", method = RequestMethod.GET)
+	public String loginStep2(Model model) {
+		return "member/loginStep2";
 	}
 	
 }
