73aab5077fd8e75ee31088c1031ef036341b17f7
hi11218 <hi11218@naver.com>
2013-08-27 17:07:16 +0900

    brandcrumb update

--- src/main/java/com/bgg/storyfarm/common/StoryfarmConstants.java
+++ src/main/java/com/bgg/storyfarm/common/StoryfarmConstants.java
@@ -17,30 +17,53 @@ public class StoryfarmConstants {
 	public final static int BREADCRUMB_FINDPWD_RESULT = 221;
 	
 	public final static int BREADCRUMB_REGISTER = 300;
-	public final static int BREADCRUMB_JOINTEP2 = 310;
-	public final static int BREADCRUMB_JOINTEP3 = 320;
+	public final static int BREADCRUMB_JOINSTEP2 = 310;
+	public final static int BREADCRUMB_JOINSTEP3 = 320;
 	
 	public final static int BREADCRUMB_STORYFARM_INTRO = 410;
 	public final static int BREADCRUMB_PUBLISHER_INTRO = 420;
 	public final static int BREADCRUMB_PARENTSROOM_INTRO = 430;
 	public final static int BREADCRUMB_PAYMENT_INFO = 440;
 	
-	public final static int BREADCRUMB_CSCENTER = 501;
-	public final static int BREADCRUMB_CSCENTER_NOTI = 502;
+	public final static int BREADCRUMB_KOREANLANGUAGE = 500;
 	
-	public final static int BREADCRUMB_CHILDREN_ROOM = 600;
-	public final static int BREADCRUMB_CHILDREN_PLAYER = 601;
+	public final static int BREADCRUMB_MYPAGE_INFO = 600;
+	public final static int BREADCRUMB_MYPAGE_PAYMENT = 601;
+	public final static int BREADCRUMB_MYPAGE_PAYMENT_SELECT = 602;
+	public final static int BREADCRUMB_MYPAGE_PAYMENT_RESULT = 603;
+	public final static int BREADCRUMB_MYPAGE_PURCHASINGINFO = 610;
+	public final static int BREADCRUMB_MYPAGE_RECEIPTISSUING = 611;
+	public final static int BREADCRUMB_MYPAGE_PURCHASINGINFO_PAST = 612;
+	public final static int BREADCRUMB_MYPAGE_COUPON = 620;
+	public final static int BREADCRUMB_MYPAGE_ADD_COUPON = 621;
+	public final static int BREADCRUMB_MYPAGE_PAUSEREQUEST = 630;
+	public final static int BREADCRUMB_MYPAGE_PAUSEREQUEST_RESULT = 631;
+	public final static int BREADCRUMB_MYPAGE_PAUSECANCEL = 632;
+	public final static int BREADCRUMB_MYPAGE_PAUSECANCEL_RESULT = 633;
+	public final static int BREADCRUMB_MYPAGE_QUESTION = 640;
+	public final static int BREADCRUMB_MYPAGE_QUESTION_DETAIL = 641;
+	public final static int BREADCRUMB_MYPAGE_QUESTION_INSERT = 642;
+	public final static int BREADCRUMB_MYPAGE_QUESTION_UPDATE = 643;
+	public final static int BREADCRUMB_MYPAGE_USERINFO = 650;
+	public final static int BREADCRUMB_MYPAGE_USERINFO_UPDATE = 651;
+	public final static int BREADCRUMB_MYPAGE_LEAVE = 660;
+	public final static int BREADCRUMB_MYPAGE_LEAVE_RESULT = 661;
 	
-	public final static int BREADCRUMB_PARENTS_ROOM = 700;
-	public final static int BREADCRUMB_PARENTS_STUDY = 701;
-	public final static int BREADCRUMB_PARENTS_CHILDREN = 702;
-	public final static int BREADCRUMB_PARENTS_TIME = 703;
-	public final static int BREADCRUMB_PARENTS_ITEMS = 704;
-	public final static int BREADCRUMB_PARENTS_SERIES = 705;
-	public final static int BREADCRUMB_PARENTS_PERIOD = 706;
-	public final static int BREADCRUMB_PARENTS_WEEKLY = 707;
-	public final static int BREADCRUMB_PARENTS_MONTHLY = 708;
+	public final static int BREADCRUMB_CHILDREN_ROOM = 700;
+	public final static int BREADCRUMB_CHILDREN_PLAYER = 701;
 	
+	public final static int BREADCRUMB_PARENTS_ROOM = 800;
+	public final static int BREADCRUMB_PARENTS_STUDY = 801;
+	public final static int BREADCRUMB_PARENTS_CHILDREN = 802;
+	public final static int BREADCRUMB_PARENTS_TIME = 803;
+	public final static int BREADCRUMB_PARENTS_ITEMS = 804;
+	public final static int BREADCRUMB_PARENTS_SERIES = 805;
+	public final static int BREADCRUMB_PARENTS_PERIOD = 806;
+	public final static int BREADCRUMB_PARENTS_WEEKLY = 807;
+	public final static int BREADCRUMB_PARENTS_MONTHLY = 808;
+
+	public final static int BREADCRUMB_CSCENTER = 901;
+	public final static int BREADCRUMB_CSCENTER_NOTI = 902;
 	
 	//BOARD
 	public final static String BOARD_ID = "board_id";
